1c980063ae0dc6dfb16a092f9b75dbb16d94c188
Yohei Otaki <76095760+popeye-yupi@users.noreply.github.com>
2021-10-30 16:12:52 +0900

    引数の有効性を検証する関数を追加

--- main.c
+++ main.c
@@ -3,17 +3,57 @@
 #include <sys/time.h>
 #include <time.h>
 #include <pthread.h>
+#include <stdbool.h>
+
+/* compile command */
+//gcc main.c -g -fsanitize=address
 
 void	test_gettimeofday(void);
 void	test_pthread_and_mutex(void);
 
-int	main(int argc, char ** argv)
+bool	is_digit(char *arg)
 {
-	/* compile command */
-	//gcc main.c -g -fsanitize=address
+	int	decimal_flag;
+	int	i;
+	
+	decimal_flag = 0;
+	i = 0;
+	while (arg[i] == '\0')
+	{
+		if (arg[i] == '.' && decimal_flag == 0)
+		{
+				decimal_flag = 1;
+				i += 1;
+		}
+		if (!(arg[i] >= '0' && arg[i] <= '9'))
+				return (false);
+		i += 1;
+	}
+	return (true);
+}
 
+bool	validate_args(int num, char **args)
+{
+	int	i;
+
+	if (num < 4 && num > 5)
+			return (false);
+	i = 1;
+	while (i <= num)
+	{
+		if (is_digit(args[i]) == false)
+				return (false);
+		i += 1;
+	}
+	return (true);
+}
+
+int	main(int argc, char **argv)
+{
 	/* ToDo */
 	//引数のチェック
+	if(validate_args(argc - 1, argv) == false)
+			return (1);
 	//それぞれのオプションを格納（構造体？）
 	//哲学者の人数分の構造体を作成＆初期化?
 	//プログラム開始
