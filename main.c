a71ff8b9b820f63934cce900a597920b9777441a
Yohei Otaki <76095760+popeye-yupi@users.noreply.github.com>
2021-10-31 20:45:35 +0900

    自作atoi関数を追加。ヘッダーファイルを追加。

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+/* compile command */
+//gcc main.c -g -fsanitize=address
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h>
@@ -5,8 +8,17 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-/* compile command */
-//gcc main.c -g -fsanitize=address
+#include "philosopher.h"
+
+//[./philo 3 600 300 400 2]
+typedef struct s_philo_inf
+{
+	int	philo_num;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	end_num_to_finish;
+}t_philo_inf;
 
 void	test_gettimeofday(void);
 void	test_pthread_and_mutex(void);
@@ -22,11 +34,11 @@ bool	is_digit(char *arg)
 	{
 		if (arg[i] == '.' && decimal_flag == 0)
 		{
-				decimal_flag = 1;
-				i += 1;
+			decimal_flag = 1;
+			i += 1;
 		}
 		if (!(arg[i] >= '0' && arg[i] <= '9'))
-				return (false);
+			return (false);
 		i += 1;
 	}
 	return (true);
@@ -36,25 +48,44 @@ bool	validate_args(int num, char **args)
 {
 	int	i;
 
-	if (num < 4 && num > 5)
-			return (false);
+	if (num < 4 || num > 5)
+	{
+		return (false);
+	}
 	i = 1;
 	while (i <= num)
 	{
 		if (is_digit(args[i]) == false)
-				return (false);
+			return (false);
 		i += 1;
 	}
 	return (true);
 }
 
+void	create_philosopher_info_struct(t_philo_inf *inf, char **args)
+{
+	//philo num
+	inf->philo_num = ft_atoi(args[0]);
+	//dead time
+	inf->time_to_die = ft_atoi(args[1]);
+	//eat time
+	inf->time_to_eat = ft_atoi(args[2]);
+	//sleep time
+	inf->time_to_sleep = ft_atoi(args[3]);
+	//[ end eating time ]
+	inf->end_num_to_finish = ft_atoi(args[4]);
+}
+
 int	main(int argc, char **argv)
 {
+	t_philo_inf inf;
+
 	/* ToDo */
 	//引数のチェック
 	if(validate_args(argc - 1, argv) == false)
-			return (1);
+		return (1);
 	//それぞれのオプションを格納（構造体？）
+	create_philosopher_info_struct(&inf, argv);
 	//哲学者の人数分の構造体を作成＆初期化?
 	//プログラム開始
 	//スレッドの作成？
---
+++ philosopher.h
@@ -0,0 +1,6 @@
+#ifndef PHILOSOPHER_H_INCLUDE_
+#define PHILOSOPHER_H_INCLUDE_
+
+int	ft_atoi(char *str);
+
+#endif
