70d1cfbe22406207d9f9c3f3d235a9f498264832
Yohei Otaki <76095760+popeye-yupi@users.noreply.github.com>
2021-11-30 14:30:57 +0900

    ヘッダーファイルをノームに対応。食事回数を多く数えてしまうバグを修正。

--- src/philos_action.c
+++ src/philos_action.c
@@ -6,7 +6,7 @@
 /*   By: yootaki <yootaki@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 23:32:55 by yootaki           #+#    #+#             */
-/*   Updated: 2021/11/26 13:15:53 by yootaki          ###   ########.fr       */
+/*   Updated: 2021/11/30 14:26:28 by yootaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,6 @@ void	few_seconds_sleep(long after_time)
 bool	philo_eat(t_philos *philo)
 {
 	pthread_mutex_lock(&(philo->info->mut_action));
-	philo->info->eat_num += 1;
-	philo->last_eat_time = get_timestamp();
 	if (philo->info->status == FINISH)
 	{
 		pthread_mutex_unlock(&(philo->mut_fork));
@@ -46,6 +44,8 @@ bool	philo_eat(t_philos *philo)
 		pthread_mutex_unlock(&(philo->info->mut_action));
 		return (false);
 	}
+	philo->info->eat_num += 1;
+	philo->last_eat_time = get_timestamp();
 	print_philo_action(philo->last_eat_time, philo->id, EAT);
 	pthread_mutex_unlock(&(philo->info->mut_action));
 	few_seconds_sleep(philo->last_eat_time + philo->info->time_to_eat);
