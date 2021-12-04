2d315ed3e3d8ad5adb6f799a2495052fff81dec9
Yohei Otaki <76095760+popeye-yupi@users.noreply.github.com>
2021-12-05 07:30:26 +0900

    パラメータとして0が渡された際にセグフォするバグを修正

--- src/ft_isdigit.c
+++ src/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: yootaki <yootaki@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 22:22:34 by yootaki           #+#    #+#             */
-/*   Updated: 2021/12/05 07:22:10 by yootaki          ###   ########.fr       */
+/*   Updated: 2021/12/05 07:29:20 by yootaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,12 @@ bool	is_digit(char *arg)
 	sign = 1;
 	result = 0;
 	i = 0;
-	while (arg[i] == ' ' || (arg[i] >= 7 && arg[i] <= 13))
-		i++;
 	if (arg[i] == '-')
-	{
 		return (false);
-	}
 	else if (arg[i] == '+')
 		i++;
+	if (arg[i] == '0')
+		return (false);
 	while (arg[i] != '\0' && (arg[i] >= '0' && arg[i] <= '9'))
 	{
 		check = ft_ovcheck(sign, result, arg[i] - '0');
